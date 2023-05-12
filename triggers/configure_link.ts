f6a5638ed3c7afeeda2f68bcca76e01ebf1737fe
Kazuhiro Sera <seratch@gmail.com>
2023-05-12 00:47:29 -0700

    Upgrade deps

--- triggers/configure_link.ts
+++ triggers/configure_link.ts
@@ -1,11 +1,15 @@
 import { Trigger } from "deno-slack-api/types.ts";
+import {
+  TriggerContextData as data,
+  TriggerTypes as type,
+} from "deno-slack-api/mod.ts";
 import workflowDef from "../workflows/configure.ts";
 
 const trigger: Trigger<typeof workflowDef.definition> = {
-  type: "shortcut",
+  type: type.Shortcut,
   name: "Configurator for ChatGPT app",
   workflow: `#/workflows/${workflowDef.definition.callback_id}`,
-  inputs: { interactivity: { value: "{{data.interactivity}}" } },
+  inputs: { interactivity: { value: data.Shortcut.interactivity } },
 };
 
 export default trigger;
